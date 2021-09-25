342d35642e221272834b8d947a9519c63130bfb4
deficts <diegoar7@live.com>
2021-09-25 17:37:18 -0500

    merge master

--- server/routes/dish.js
+++ server/routes/dish.js
@@ -2,36 +2,71 @@ const router = require('express').Router()
 const Dish = require('../models/Dish')
 const Chef = require('../models/Chef')
 const User = require('../models/User')
-
-
+const AWS = require('aws-sdk');
+const { ACCESS_KEY_ID, SECRET_ACCESS_KEY, AWS_REGION, S3_BUCKET } = process.env
 
 // @route   POST /api/dish/create
 // @desc    Create a new dish
 // @access  Public
 router.post('/create', async (req, res) => {
     try {
-
         // check if ID of chef related to the dish exists
-        const chefExists = await Chef.findById(req.body.chefID)
-        if (!chefExists) { return res.status(500).json({ message: err})}
+        const chefExists = await User.findById(req.body.chefID)
+        if (!chefExists) { 
+            return res.status(500).json({ message: 'El chef no existe'})
+        }
+
+        let image = null;
+
+        if (req.body.base64) {
+            AWS.config.update({
+                accessKeyId: ACCESS_KEY_ID,
+                secretAccessKey: SECRET_ACCESS_KEY,
+                region: AWS_REGION
+            })
+
+            const s3 = new AWS.S3()
+
+            const base64Data = new Buffer.from(req.body.base64.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+
+            const params = {
+                Bucket: S3_BUCKET,
+                Body: base64Data,
+                ACL: 'public-read',
+                ContentEncoding: 'base64',
+                ContentType: `image/jpeg`,
+                Key: `${new Date().toISOString()}.jpeg`
+            }
+
+            try {
+                const { Location } = await s3.upload(params).promise();
+                image = Location;
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({ message: 'Error al obtener el ', error })
+            }
+        }
 
         // Add the new Dish
         const dish = new Dish({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
-            chefID: req.body.chefID
+            chefID: req.body.chefID,
+            image: image
         })
 
         // Save in DB
         await dish.save().catch(
             (error) => {
+                console.log(error);
                 res.status(400).json({ message: "No se pudo crear el platillo", error})
             }
         )
         res.status(200).json({ message: "Platillo creado con éxito" })
 
     } catch (err) {
+        console.log(err);
         res.status(500).json({ message: err })
     }
 
@@ -75,16 +110,15 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-
 // @route   Get /api/dish/dishes
 // @desc    Get all dishes from a chef
 // @access  Private
-
 router.get('/dishes', async (req, res) => {
     try {
         const entry = await Dish.find({})
         var exportDishes = JSON.parse(JSON.stringify(entry))
         for (let i = 0; i < entry.length; i++){
+            console.log(entry[i].chefID)
             let user = User.findById(entry[i].chefID)
             exportEntry.user = user
         }
@@ -94,21 +128,5 @@ router.get('/dishes', async (req, res) => {
     }
 })
 
+module.exports = router
 
-// @route   Get /api/dish/dishes
-// @desc    Get all dishes from a chef
-// @access  Private
-
-router.get('/dishes', async (req, res) => {
-    try {
-        const entry = await Dish.find({})
-        var exportEntry = JSON.parse(JSON.stringify(entry))
-        for (let i = 0; i < entry.length; i++){
-            let chef = Chef.findById(entry[i].chefID)
-            exportEntry.chef = chef
-        }
-        res.json(exportEntry)
-    } catch (err) {
-        res.status(500).json({ message: err })
-    }
-})
